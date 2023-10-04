6636c6f24e402ba4bfab03aa27fdd800092fd428
KoneJ <kbvv67890@gmail.com>
2023-10-04 18:19:04 +0900

    포스트 목록 불러오

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,9 +1,17 @@
 import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
-  useCdn: false, // 동적인 데이터 이므로 false
+  useCdn: false,
   apiVersion: '2023-09-24',
   token: process.env.SANITY_SECRET_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source).width(800).url();
+}
